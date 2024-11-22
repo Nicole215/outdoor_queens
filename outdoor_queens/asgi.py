ff525b6214d5da7aab294fc647d7696b592c6dc0
Nicole Burmann <daria215@gmail.com>
2024-11-22 12:19:04 +0000

    Initial commit

---
+++ outdoor_queens/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for outdoor_queens project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outdoor_queens.settings')
+
+application = get_asgi_application()
