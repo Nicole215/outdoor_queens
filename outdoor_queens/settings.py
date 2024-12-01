8c9bac1cfbf582a45071ac1c516044702f8d5a3e
Nicole Burmann <daria215@gmail.com>
2024-12-01 16:59:08 +0000

    Add add to bag funtionality

--- outdoor_queens/settings.py
+++ outdoor_queens/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '=ue6v0!xtiwrjkdv11(ml99&((zsoo+4$6rov=3w=fu6fzgec(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-nicole215-outdoorqueens-ga3ht18uwhi.ws.codeinstitute-ide.net', 'outdoor-queens-e3117004e229.herokuapp.com', 'localhost']
 CSRF_TRUSTED_ORIGINS = [
