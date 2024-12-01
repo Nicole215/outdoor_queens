6da92f047535446a7496e025841fc5ed14614dbe
Nicole Burmann <daria215@gmail.com>
2024-12-01 10:35:11 +0000

    Set secret key

--- outdoor_queens/settings.py
+++ outdoor_queens/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '=ue6v0!xtiwrjkdv11(ml99&((zsoo+4$6rov=3w=fu6fzgec(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-nicole215-outdoorqueens-ga3ht18uwhi.ws.codeinstitute-ide.net', 'outdoor-queens-e3117004e229.herokuapp.com']
+ALLOWED_HOSTS = ['8000-nicole215-outdoorqueens-ga3ht18uwhi.ws.codeinstitute-ide.net', 'outdoor-queens-e3117004e229.herokuapp.com', 'localhost']
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-nicole215-outdoorqueens-ga3ht18uwhi.ws.codeinstitute-ide.net'
 ]
