c4aee5f774b3b8ab33f83fa1e65255f430187724
Nicole Burmann <daria215@gmail.com>
2024-12-01 13:00:05 +0000

    Add cache control

--- outdoor_queens/settings.py
+++ outdoor_queens/settings.py
@@ -161,7 +161,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
@@ -169,6 +168,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'outdoor-queens'
     AWS_S3_REGION_NAME = 'eu-north-1'
