e9a73a99c4aa0731846dbed4460de6dd13369d9c
Nicole Burmann <daria215@gmail.com>
2024-12-04 12:34:42 +0000

    Add checkout views and templates

--- outdoor_queens/settings.py
+++ outdoor_queens/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
     # Other
     'storages',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'outdoor_queens.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -83,8 +86,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
